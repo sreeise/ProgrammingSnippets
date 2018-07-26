3f137ab4831a4ac077bc231836f3a4ea92ed7665
Sean Reeise <reeisesean@gmail.com>
2018-07-26 04:30:18 -0400

    Array Rotation class and left rotation method

---
+++ Java/src/JavaReference.java
@@ -0,0 +1,3 @@
+public class JavaReference {
+  public static void main(String[] args) {}
+}
---
+++ Java/src/arrys/ArrayRotate.java
@@ -0,0 +1,34 @@
+package arrys;
+
+public class ArrayRotate {
+
+  private ArrayRotate() {}
+
+  /**
+   * Returns a copy of an int array with its integers rotated
+   * to the left a given amount of times.
+   * @param array The array given for rotation
+   * @param amount The amount of times to rotate
+   * @return Array of int's
+   */
+  public static int[] left(int[] array, int amount) {
+    for (int i = 0; i < amount; i++) {
+      int first = array[0];
+      array = copy(array);
+      array[array.length -1] = first;
+    }
+    return array;
+  }
+
+  /**
+   * Copies an array to a new array starting at position 1.
+   * Copied array has the same length.
+   * @param array The array to be copied
+   * @return Array of int's copied from given array starting at position 1
+   */
+  private static int[] copy(int[] array) {
+    int[] array2 = new int[array.length];
+    System.arraycopy(array, 1, array2, 0, array.length - 1);
+    return array2;
+  }
+}
