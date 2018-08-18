0328fed7b1731f67663e16c21e7044b3daef48c6
Sean Reeise <reeisesean@gmail.com>
2018-08-17 21:00:38 -0400

    Change 0N2 runtime method to correct name.

--- Java/src/RunTime/ON2Runtime.java
+++ Java/src/RunTime/ON2Runtime.java
@@ -10,7 +10,9 @@ public class ON2Runtime {
   // Here the inner loop with variable j will loop for each value of
   // of the array so it is O(N) and then it does this for every
   // value in the array based upon the outer loop so it becomes O(N^2).
-  public static void O1(int[] array) {
+  // Since we are only concerned with the worst case, the O(N^2) becomes
+  // the overwhelming factor, thus our algorithm worst case runtime is 0(n^2).
+  public static void ON2(int[] array) {
     for (int i = 0; i < array.length; i++) {
       for (int j = 0; j < array.length; j++) {
         System.out.println(array[i] + ", " + array[j]);
