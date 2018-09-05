01c3dce756d4857a80d98d46c8f3d12b4446e9e1
Sean Reeise <reeisesean@gmail.com>
2018-09-05 02:27:39 -0400

    Generics and Wildcards

---
+++ Java/src/data/collections/GenWild.java
@@ -0,0 +1,17 @@
+package data.collections;
+
+// This class is used to represent examples in
+// other classes in this package
+
+
+public class GenWild<T> {
+  private T[] array;
+
+  public GenWild(T[] array) {
+    this.array = array;
+  }
+
+  void add(T b) {
+    array[array.length -1] = b;
+  }
+}
---
+++ Java/src/data/collections/GenericsAndWildcards.java
@@ -0,0 +1,66 @@
+package data.collections;
+
+import java.util.Collection;
+
+public class GenericsAndWildcards {
+/*
+Notes
+
+More info on generics can be found at https://docs.oracle.com/javase/tutorial/extra/generics/index.html
+
+Lower bound wildcards: ? super T
+Upper bound wildcards: ? extends T
+Wildcard capture: Such as ? in List<?> where the type of ? is inferred by the compiler
+
+T in generics stands for the type that the Class object is representing
+ */
+
+  /**
+   * Print objects in a collection. Because we use the wildcard: ?,
+   * we are able to print any collection regardless of type.
+   * @param c A collection of any type
+   */
+  public void print(Collection<?> c) {
+    for (Object e : c) {
+      System.out.println(e);
+    }
+  }
+
+  /**
+   * Add objects of any type from a collection to an array through
+   * the use of generic types. This could not be done if the array
+   * were specified as an Object, only as a generic.
+   * @param array Array of any type
+   * @param collection Collection of any type
+   * @param <T> Type for the array and collection
+   */
+  static <T> void toArray(T[] array, Collection<T> collection) {
+    for (T each : array) {
+      collection.add(each);
+    }
+  }
+
+
+  /**
+   * For demonstrative purposes only.
+   * Array out of bounds errors could be thrown here.
+   * This will work for arrays that have room for the element
+   * or a dynamically resizable array.
+   *
+   * Lower bounded wildcard using keyword super.
+   * Adds the last element in the collection to the array
+   * in class GenWild
+   * @param coll Collection of any type
+   * @param genWild Wild card of any t ype
+   * @param <T> Type
+   * @return Last element in the collection
+   */
+  public static <T> T getMethod(Collection<T> coll, GenWild<? super T> genWild) {
+    T element = null;
+    for (T t : coll) {
+      element = t;
+      genWild.add(element);
+    }
+    return element;
+  }
+}
