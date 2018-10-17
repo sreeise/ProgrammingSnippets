408e8ebb145fac7498878caf1c524e1d23bbe568
sreeise <reeisesean@gmail.com>
2018-10-16 20:53:12 -0400

    Added JS for basics of Object.defineProperty

--- JavaScript/objects/object_define_property.js
+++ JavaScript/objects/object_define_property.js
@@ -1,3 +1,9 @@
+/*
+Object.defineProperty
+
+MDN docs on Object.defineProperty:
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
+ */
 
 function Computer(brand) {
   this.brand = brand;
@@ -15,12 +21,20 @@ function Mac(cpu, ram) {
   this.ram = ram;
 }
 
+// Extend the prototype of Computer using allowing
+// us to call methods and use variables from Computer
+// in the Mac object. See prototypes directory.
+Mac.prototype = Object.create(Computer.prototype);
+
 // Using define property to create a getter
+// This uses the name given: 'specs', as the getter
+// method for the String in the get() method
 Object.defineProperty(Mac.prototype, "specs", {
   get() {
-    return `CPU: ${this.cpu}, RAM: ${this.ram}`;
+    return `BRAND: ${this.brand} CPU: ${this.cpu}, RAM: ${this.ram}`;
   }
 });
 
 let macbook = new Mac("i7 8700", "16GB");
-console.log(macbook.specs);
+macbook.brandType(); // --> Mac
+console.log(macbook.specs); // --> BRAND: Mac CPU: i7 8700, RAM: 16GB
