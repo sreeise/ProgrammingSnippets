b72212dfc646ce4f756ea4fc9f9a0b6171814077
sreeise <reeisesean@gmail.com>
2018-11-16 14:11:47 -0500

    Added dequeClass and updated README.md for license.

---
+++ Java/src/data/stack/DequeBrackets.java
@@ -0,0 +1,48 @@
+package data.stack;
+
+import java.util.*;
+
+public class DequeBrackets {
+
+  /**
+   * Given a string of brackets, such as `{(([])[])[]}[]`,
+   * the method checks to see if the the String is a balanced
+   * set of brackets.
+   *
+   * @param String s
+   * @return True is the string is balanced else false
+   */
+  public static boolean isBalanced(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+    char current = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (!stack.isEmpty()) {
+        current = stack.peek();
+      }
+
+      stack.push(s.charAt(i));
+
+      if (!stack.isEmpty() && stack.size() > 1) {
+        if (shouldPop(current, stack.peek())) {
+          stack.pop();
+          stack.pop();
+        }
+      }
+    }
+
+    return stack.isEmpty();
+  }
+
+  private static boolean shouldPop(char current, char next) {
+    switch (current) {
+      case '[':
+        return next == ']';
+      case '{':
+        return next == '}';
+      case '(':
+        return next == ')';
+      default:
+        return false;
+    }
+  }
+}
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Things I write down and helpful concepts in my journey as a programmer.
 
 This is by no means a complete list of any one topic. Some parts may be at a beginner level while other parts are more advanced. This is really just random Snippets/references in programming that I have found useful in various situations.
 
-License under the GNU General Public License v3.0
+License under the MIT license.
