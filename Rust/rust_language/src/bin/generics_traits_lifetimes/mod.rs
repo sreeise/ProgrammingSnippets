f955c0ffed2f228a0bab36b24992107e9ca0b5b1
sreeise <reeisesean@gmail.com>
2018-12-02 00:52:32 -0500

    Added example of a trait

--- Rust/rust_language/src/bin/generics_traits_lifetimes/mod.rs
+++ Rust/rust_language/src/bin/generics_traits_lifetimes/mod.rs
@@ -2,6 +2,7 @@
 Definitions and Code from:
 https://doc.rust-lang.org/book/2018-edition/ch10-00-generics.html
 https://doc.rust-lang.org/book/2018-edition/ch17-03-oo-design-patterns.html
+daringordon.com/authz_rs_tutorial/03.authz.rs/
 
 Generics: Abstract stand-ins for concrete types or other properties.
 
@@ -412,4 +413,75 @@ fn define_library() {
     };
 
     screen.run();
+}
+
+/*
+Example Implementation of Trait
+*/
+
+struct Sheep { naked: bool, name: &'static str }
+
+trait Animal {
+    // Static method signature; `Self` refers to the implementor type.
+    fn new(name: &'static str) -> Self;
+
+    // Instance method signatures; these will return a string.
+    fn name(&self) -> &'static str;
+    fn noise(&self) -> &'static str;
+
+    // Traits can provide default method definitions.
+    fn talk(&self) {
+        println!("{} says {}", self.name(), self.noise());
+    }
+}
+
+impl Sheep {
+    fn is_naked(&self) -> bool {
+        self.naked
+    }
+
+    fn shear(&mut self) {
+        if self.is_naked() {
+            // Implementor methods can use the implementor's trait methods.
+            println!("{} is already naked...", self.name());
+        } else {
+            println!("{} gets a haircut!", self.name);
+
+            self.naked = true;
+        }
+    }
+}
+
+// Implement the `Animal` trait for `Sheep`.
+impl Animal for Sheep {
+    // `Self` is the implementor type: `Sheep`.
+    fn new(name: &'static str) -> Sheep {
+        Sheep { name: name, naked: false }
+    }
+
+    fn name(&self) -> &'static str {
+        self.name
+    }
+
+    fn noise(&self) -> &'static str {
+        if self.is_naked() {
+            "baaaaah?"
+        } else {
+            "baaaaah!"
+        }
+    }
+
+    // Default trait methods can be overridden.
+    fn talk(&self) {
+        // For example, we can add some quiet contemplation.
+        println!("{} pauses briefly... {}", self.name, self.noise());
+    }
+}
+
+pub fn call_trait() {
+    let mut dolly: Sheep = Animal::new("Dolly");
+
+    dolly.talk();
+    dolly.shear();
+    dolly.talk();
 }
