80322227744325e5456b41ed1cf838a892f1c6fa
sreeise <reeisesean@gmail.com>
2018-12-25 08:24:34 -0500

    Moved Java project to gradle based project. Tests coverage moved to JUnit

---
+++ Java/snippet/src/main/java/runtime/O1Runtime.java
@@ -0,0 +1,16 @@
+package runtime;
+
+public class O1Runtime {
+
+  private O1Runtime() {}
+
+  // This is O(1) runtime.
+  // O(1): AS the size increases, time stays the same. It won't take longer
+  // or less time no matter the size of the data.
+  // We are always returning the same element, the zeroth element in the array,
+  // regardless of the size of data. The size of the array doesn't matter
+  // because we only care about 1 element.
+  public static void O1(int[] array) {
+    System.out.println(array[0]);
+  }
+}
---
+++ Java/snippet/src/test/java/tests/SortTest.java
@@ -0,0 +1,61 @@
+package tests;
+
+import org.junit.jupiter.api.Test;
+import sort.strcompare.CompareSort;
+import sort.strcompare.KeyValueMap;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortTest {
+  private static String[] testFiles = {"test_files/test_str_comparator/input05.txt"};
+  private static String[] outFiles = {"test_files/test_str_comparator/output05.txt"};
+
+  @Test
+  static void testCompareSort() throws FileNotFoundException {
+
+    String test_files[] = FileUtils.getPathsArray(testFiles);
+    String out_files[] = FileUtils.getPathsArray(outFiles);
+    for (int testIndex = 0; testIndex < testFiles.length; testIndex++) {
+      File file = new File(test_files[testIndex]);
+
+      Scanner scan = new Scanner(file);
+      int n = scan.nextInt();
+
+      KeyValueMap[] keyValueMap = new KeyValueMap[n];
+      CompareSort checker = new CompareSort();
+
+      for (int i = 0; i < n; i++) {
+        keyValueMap[i] = new KeyValueMap(scan.next(), scan.nextInt());
+      }
+      scan.close();
+      File output = new File(out_files[testIndex]);
+
+      Scanner scanner = new Scanner(output);
+
+
+      KeyValueMap[] keyValueMap2 = new KeyValueMap[n];
+
+      for (int j = 0; j < n; j++) {
+        keyValueMap2[j] = new KeyValueMap(scanner.next(), scanner.nextInt());
+      }
+
+      Arrays.sort(keyValueMap, checker);
+      scanner.close();
+
+      int count = keyValueMap.length - 1;
+
+      for (int i = 0; i < keyValueMap.length; i++) {
+        assertEquals(keyValueMap[i].str, keyValueMap2[count].str);
+        if (keyValueMap[i].str.equals(keyValueMap2[count].str)) {
+          assertEquals(keyValueMap[i].num, keyValueMap2[count].num);
+        }
+        count -= 1;
+      }
+    }
+  }
+}
